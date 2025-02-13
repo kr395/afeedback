7ebe7451d737ea860fa0d3eb84755ad447138217
Gopal Kumar Das <krgopalkumar706@gmail.com>
2025-02-13 12:00:00 +0530

    anonomus feedback started. initial commit. UserModel Created

---
+++ src/model/User.model.ts
@@ -0,0 +1,80 @@
+import mongoose, { Schema } from "mongoose";
+import { Document } from "mongoose";
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verified: boolean;
+  verifyCode: number;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessages: boolean;
+  messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+      "Please use a valid email address",
+    ],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  verifyCode: {
+    type: Number,
+    required: [true, "verify code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "verify code expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessages: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
+
+const User = mongoose.models.User as mongoose.Model<User> || mongoose.model<User>("User", UserSchema);
+
+export default User;
