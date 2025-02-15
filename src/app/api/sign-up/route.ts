b26c9cdcf21f0b8f14ddb3abe54586b27f83d8b2
Gopal Kumar Das <krgopalkumar706@gmail.com>
2025-02-15 16:00:00 +0530

    singup route created

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -7,7 +7,71 @@ export async function POST(request: Request) {
   await dbConnect();
   try {
     const { username, email, password } = await request.json();
-    
+
+    const existingUserVerifiedByUsername = await User.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingUserVerifiedByUsername) {
+      return Response.json(
+        { success: false, message: "Username already exists" },
+        { status: 400 }
+      );
+    }
+    const existingUserByEmail = await User.findOne({
+      email,
+    });
+    const verificationCode = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+    if (existingUserByEmail) {
+      if (existingUserByEmail.isVerified) {
+        return Response.json(
+          { success: false, message: "Email already exists" },
+          { status: 400 }
+        );
+      } else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        existingUserByEmail.password = hashedPassword;
+        existingUserByEmail.verifyCode = verificationCode;
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
+        await existingUserByEmail.save();
+      }
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+
+      const newUser = new User({
+        username,
+        email,
+        password: hashedPassword,
+        verifyCode: verificationCode,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessages: true,
+        messages: [],
+      });
+
+      await newUser.save();
+    }
+    // Send verification email
+    const emailResponse = await sendEmail(email, username, verificationCode);
+
+    if (!emailResponse.success) {
+      return Response.json(
+        { success: false, message: emailResponse.message },
+        { status: 500 }
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        message: "User registered successfully. Please verify your Email.",
+      },
+      { status: 201 }
+    );
   } catch (error) {
     console.log("Error connecting to database", error);
     return Response.json(
